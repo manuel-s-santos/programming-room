28cc5692e643e34abcc4c595679fa21eac519f26
Nadine Amanda <nadine@soar.earth>
2021-12-10 17:09:26 +0800

    added styling for workout.tsx

---
+++ src/Test/joke.tsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+
+const Joke = () => {
+  const [ joke, setJoke ] = useState('Random Joke loading ...');
+  useEffect(() => {fetch('https://v2.jokeapi.dev/joke/programming?idRange=0-100').then(response => response.json())
+      .then(data => setJoke(data.joke))
+  }, [])
+  return (<div>{ joke }</div>)
+}
+const App = () => {
+  return <Joke/>
+}
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default Joke;
