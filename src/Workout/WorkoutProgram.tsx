00b5491ce00de1ca9d6304f9212dd079c3573836
Nadine Amanda <nadine@soar.earth>
2021-12-12 12:31:07 +0800

    commented out WorkoutProgram function, try new code

--- src/Workout/WorkoutProgram.tsx
+++ src/Workout/WorkoutProgram.tsx
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 import React, {useState, useEffect} from "react";
 import WorkoutGoal from './WorkoutGoal';
 
-const WorkoutProgram = props => {
-    return (
-        <div className='workout-program'>
-            <WorkoutGoal></WorkoutGoal>
-
-        </div>
-    );
-  };
+//const WorkoutProgram = props => {
+//    return (
+//        <div className='workout-program'>
+//        </div>
+//    );
+//   };
   
 export default WorkoutProgram;
