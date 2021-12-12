fef0b6c3bc9686ba37646b36289a6a67096e66d5
Nadine Amanda <nadine@soar.earth>
2021-12-12 21:31:37 +0800

    removed WorkoutGoal component from WorkoutProgram

--- src/Workout/WorkoutProgram.tsx
+++ src/Workout/WorkoutProgram.tsx
@@ -6,7 +6,7 @@ const WorkoutProgram = () => {
     return (
         <div className='workout-program'>
              <label><h3> Workout Program Checklist </h3></label>
-             <WorkoutGoal></WorkoutGoal>
+             
         </div>
     );
    };  
