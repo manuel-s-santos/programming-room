9090dd9698dee649907cacc833807ccd2bc806c0
Nadine Amanda <nadine@soar.earth>
2021-12-12 17:52:31 +0800

    added background, modified workout folder files

--- src/Workout/WorkoutProgram.tsx
+++ src/Workout/WorkoutProgram.tsx
@@ -2,11 +2,13 @@ import styled from 'styled-components';
 import React, {useState, useEffect} from "react";
 import WorkoutGoal from './WorkoutGoal';
 
-//const WorkoutProgram = props => {
-//    return (
-//        <div className='workout-program'>
-//        </div>
-//    );
-//   };
+const WorkoutProgram = () => {
+    return (
+        <div className='workout-program'>
+        </div>
+    );
+   };
+
+
   
 export default WorkoutProgram;
