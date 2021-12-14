037c542091542111a51195b033ebf8a251865b92
Nadine Amanda <nadine@soar.earth>
2021-12-14 15:08:11 +0800

    changed workoutlist to interface and modified workoutprogram and workout goal

--- src/Workout/WorkoutProgram.tsx
+++ src/Workout/WorkoutProgram.tsx
@@ -1,21 +1,38 @@
 import React, {useState, ChangeEvent} from "react";
 // import WorkoutGoal from './WorkoutGoal';
 import  './workout.css';
+import WorkoutList from "./WorkoutList";
 
 const WorkoutProgram = () => {
-    const [benchPress, setBenchPress]=  useState<string> ("");
-    const [cableRow, setCableRow]=  useState<string> ("");
-    const [chinUps, setChinUps]=  useState<string> ("");
+    const [goal, setGoal]=  useState<string> ("");
+    const [reps, setReps] = useState<number>(3);
+    // const [cableRow, setCableRow]=  useState<string> ("");
+    // const [chinUps, setChinUps]=  useState<string> ("");
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        if (event.target.name === "benchPress"){
-            setBenchPress(event.target.value)
+        if (event.target.name === "goal"){
+            setGoal(event.target.value)
         } else {
-            alert('Congratulations!');
-            //setReps(Number(event.target.value));
+            // alert('Congratulations!');
+            setReps(Number(event.target.value));
         }
         };
 
+    const addGoal = (): void => {
+        const newGoal = { goalName: goal, reps: reps };
+        setWorkoutGoal([...workoutList, newGoal]);
+        setGoal("");
+        setReps(0);
+    };
+        const completeGoal = (goalNameToDelete: string): void => {
+            set workoutList(
+                workoutList.filter((task) => {
+                    return goal.goalName != goalNameToDelete;
+                })
+            );
+
+        };
+        
     return (
         <div className='workout-program'>
              <label><h3> Workout Program Checklist </h3></label>
