497ca51cf00f5088e5e74dcfa81fb05f657b69c1
Nadine Amanda <nadine@soar.earth>
2021-12-14 13:58:04 +0800

    modified WorkoutProgram.tsx(added input types etc)

--- src/Workout/WorkoutProgram.tsx
+++ src/Workout/WorkoutProgram.tsx
@@ -1,15 +1,28 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, ChangeEvent} from "react";
 // import WorkoutGoal from './WorkoutGoal';
 import  './workout.css';
 
 const WorkoutProgram = () => {
+    const [benchPress, setBenchPress]=  useState<string> ("");
+    const [cableRow, setCableRow]=  useState<string> ("");
+    const [chinUps, setChinUps]=  useState<string> ("");
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.name === "benchPress"){
+            setBenchPress(event.target.value)
+        } else {
+            alert('Congratulations!');
+            //setReps(Number(event.target.value));
+        }
+        };
+
     return (
         <div className='workout-program'>
              <label><h3> Workout Program Checklist </h3></label>
              <div className="workout-container">
-             <input type="text" placeholder="Bench press" name="Bench press" value="Bench Press"/>
-             <input type="text" placeholder="Cable Row" name="Cable Row" value="Cable Row" />
-             <input type="text" placeholder="Assisted Chin Ups" name="Assisted Chin Ups" value="Assisted Chin Ups"/>
+             <input type="text" placeholder="Bench press" value={benchPress} onChange={handleChange} />
+             <input type="text" placeholder="Cable Row" value={cableRow} onChange={handleChange} />
+             <input type="text" placeholder="Assisted Chin Ups" value={chinUps} onChange={handleChange} />
              </div>
             </div>
     );
