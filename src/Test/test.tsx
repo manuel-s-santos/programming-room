f2fbaf4cdc64dadea35862293d9edb9421a38617
Nadine Amanda <nadine@soar.earth>
2021-12-14 11:42:27 +0800

    renamed test.module.css to test.css. adjusted to fir workout program

--- src/Test/test.tsx
+++ src/Test/test.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
+import '../Test/test.css';
 
 
 const Test = () =>  {
     const [count, setCount] = useState(0);
     return (
       <div className="Test">
+        <button className='pixel' onClick={() => setCount(count +1)}>Increment</button> 
         <h3> {count} </h3><br/>
-        <button className='pixel' onClick={() => setCount(count +1)}>Increment</button>
         <button className='pixel' onClick={() => setCount(count -1)}>Decrement</button>
       </div>
       
